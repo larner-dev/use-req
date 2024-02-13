531f69a6bbf114ffcea02a6a7bab0cc01ab59318
Aaron Larner <835542+alarner@users.noreply.github.com>
2024-02-13 14:58:01 -0800

    Improve error handling

--- src/useReq.ts
+++ src/useReq.ts
@@ -1,18 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Req, ReqMethod } from "@larner.dev/req";
+import { Req, ReqMethod, ReqCode } from "@larner.dev/req";
 import { useMemo, useState } from "react";
 
-interface UseReqState<T, E extends string = string> {
+interface ReqError<T extends string> {
+  message: string;
+  code: ReqCode | T;
+  params?: Record<string, any>;
+}
+
+interface UseReqState<T, E extends string> {
   loading: boolean;
-  error: {
-    message: string;
-    code: E;
-    params?: Record<string, any>;
-  } | null;
+  error: ReqError<E> | null;
   result: T | null;
 }
 
-export interface ExtendedUseReqState<T> extends UseReqState<T> {
+export interface ExtendedUseReqState<T, E extends string>
+  extends UseReqState<T, E> {
   clearError: () => void;
 }
 
@@ -23,8 +26,24 @@ export interface RequestMethods_T<T> {
   delete: (url: string, params?: any) => void | T | null;
 }
 
-export const useReq = <T>(request: Req) => {
-  const [state, setState] = useState<UseReqState<T>>({
+interface AbstractResult {
+  success: boolean;
+}
+
+interface SuccessResult<T> extends AbstractResult {
+  success: true;
+  result: T;
+}
+
+interface ErrorResult<E extends string> extends AbstractResult {
+  success: false;
+  error: ReqError<E>;
+}
+
+type ReqResult<T, E extends string> = SuccessResult<T> | ErrorResult<E>;
+
+export const useReq = <T, E extends string = string>(request: Req) => {
+  const [state, setState] = useState<UseReqState<T, E>>({
     loading: false,
     error: null,
     result: null,
@@ -35,23 +54,24 @@ export const useReq = <T>(request: Req) => {
       method: ReqMethod,
       url: string,
       params: any
-    ) => {
+    ): Promise<ReqResult<T, E>> => {
       setState({ error: null, loading: true, result: null });
       try {
         const result = await request[method](url, params);
         setState({ error: null, loading: false, result });
-        return result as T;
+        return { success: true, result: result as T };
       } catch (error: any) {
+        const formattedError = {
+          message: error.message,
+          code: error.code || "UNKNOWN",
+          params: error.params || undefined,
+        };
         setState({
-          error: {
-            message: error.message,
-            code: error.code || "UNKNOWN",
-            params: error.params || undefined,
-          },
+          error: formattedError,
           loading: false,
           result: null,
         });
-        return null;
+        return { success: false, error: formattedError };
       }
     };
 
@@ -72,6 +92,6 @@ export const useReq = <T>(request: Req) => {
     {
       ...state,
       clearError: () => setState({ ...state, error: null }),
-    } as ExtendedUseReqState<T>,
+    } as ExtendedUseReqState<T, E>,
   ] as const;
 };
